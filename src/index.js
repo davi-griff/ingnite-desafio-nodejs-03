846e84711e0d0debe6333ce4d559f1bde34592ef
Davi Araujo <davi.amaral.araujo@gmail.com>
2021-03-19 11:04:03 -0300

    finaly

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  const repositoryIndex = respositories.findIndex((repository) => repository.id === id);
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
   if (repositoryIndex < 0){
     return response.status(404).json({ error:"Repository not found"});
   };
